7cf1309299fea6d361fc2e38cb271fc62340dd92
webb-orbit <webbenemies@gmail.com>
2024-07-04 20:18:23 +0530

    log

--- src/appwriteapi/authentic.js
+++ src/appwriteapi/authentic.js
@@ -1,4 +1,4 @@
-import { Client, Account, ID, Avatars} from "appwrite";
+import { Client, Account, ID, Avatars } from "appwrite";
 import appwriteconf from "../config/conf";
 
 class author {
@@ -11,64 +11,63 @@ class author {
             .setProject(appwriteconf.projectid);
         this.account = new Account(this.client)
         this.avatar = new Avatars(this.client)
-
     }
 
-    async getcurrentuser(){
+    async getcurrentuser() {
         return await this.account.get()
     }
 
-   async firstcreate(email, password, name){
-     let creation = await this.account.create(ID.unique(),email, password, name)
-     if (creation) {
-        return await this.login(email, password)
-     }
-   }
+    async firstcreate(email, password, name) {
+        let creation = await this.account.create(ID.unique(), email, password, name)
+        if (creation) {
+            return await this.login(email, password)
+        }
+    }
 
-   async createvaryfy(){
-    return await this.account.createVerification("http://localhost:5173/Verification")
-   }
+    async createvaryfy() {
+        return await this.account.createVerification("http://localhost:5173/Verification")
+    }
 
-   async Verifyuser(userid, secret){
-    return await this.account.updateVerification(userid, secret)
-   }
+    async Verifyuser(userid, secret) {
+        return await this.account.updateVerification(userid, secret)
+    }
 
-   async login(email, password){
-    return await this.account.createEmailPasswordSession(email, password)
-   }
+    async login(email, password) {
+        return await this.account.createEmailPasswordSession(email, password)
+    }
 
-//    passwordrecovery
-async changepassword(passemail){
-    return await this.account.createRecovery(passemail, "http://localhost:5173/changepassword")
-}
+    //    passwordrecovery
+    async changepassword(passemail) {
+        return await this.account.createRecovery(passemail, "http://localhost:5173/changepassword")
+    }
 
-async updatepassword(userid, secret, newpassword, repete){
-    return await this.account.updateRecovery(userid, secret, newpassword, repete)
-}
-//    passwordrecovery
+    async updatepassword(userid, secret, newpassword, repete) {
+        return await this.account.updateRecovery(userid, secret, newpassword, repete)
+    }
+    //    passwordrecovery
 
-async appwritelogout(){
-return await this.account.deleteSession("current")
-}
+    async appwritelogout() {
+        return await this.account.deleteSession("current")
+    }
 
-async getavatar(name){
-    return await this.avatar.getInitials(name)
-}
+    async getavatar(name) {
+        return await this.avatar.getInitials(name)
+    }
 
-async updateemail(email, password){
-    return await this.account.updateEmail(email, password)
-}
+    async updateemail(email, password) {
+        return await this.account.updateEmail(email, password)
+    }
 
-async updatename(newname){
-    return await this.account.updateName(newname)
-}
+    async updatename(newname) {
+        return await this.account.updateName(newname)
+    }
 
-async logoutall(){
-    return await this.account.deleteSessions()
-}
-async listsession(){
-    return await this.account.listSessions()
-}
+    async logoutall() {
+        return await this.account.deleteSessions()
+    }
+    async listsession() {
+        return await this.account.listSessions()
+    }
 
 }
 
